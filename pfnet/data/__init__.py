d20a590f6f7a0ae7d85554e54d4e3315cd91b009
jbarberia <50841177+jbarberia@users.noreply.github.com>
2019-10-04 14:39:01 -0300

    Add files via upload

---
+++ pfnet/data/__init__.py
@@ -0,0 +1,14 @@
+#***************************************************#
+# This file is part of PFNET.                       #
+#                                                   #
+# Copyright (c) 2015, Tomas Tinoco De Rubira.       #
+#                                                   #
+# PFNET is released under the BSD 2-clause license. #
+#***************************************************#
+
+IEEE_14_BUS  = "IEEE 14 bus.raw"
+IEEE_30_BUS  = "IEEE 30 bus.raw"
+IEEE_39_BUS  = "IEEE 39 bus.raw"
+IEEE_118_BUS = "IEEE 118 bus.raw"
+VER_1718_PID = "ver1718pid.raw"
+VER_1718_PID_33 = "ver1718pid_33"
