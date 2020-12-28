4c46388e6a2d4ad64bead8bcec878153ebe47223
Tomas Tinoco De Rubira <ttinoco5687@gmail.com>
2020-12-27 18:02:48 -0800

    testing mvsc

--- appveyor/configure_conda_and_install_pfnet.ps1
+++ appveyor/configure_conda_and_install_pfnet.ps1
@@ -1,6 +1,4 @@
 conda create -n testenv --yes python=$env:PYTHON_VERSION pip
 conda activate testenv
 "python=$env:PYTHON_VERSION" | Out-File C:\conda\envs\testenv\conda-meta\pinned -encoding ascii
-conda install -c anaconda --yes libpython
-conda install -c msys2 --yes m2w64-toolchain
 pip install -r requirements.txt
